b372aac3a5f3e02dc909d69e2196916c5db88511
devcsb <devcsb119@gmail.com>
2022-06-25 21:11:01 +0900

    Initial commit

---
+++ src/main/java/com/csb/simplemall/SimplemallApplication.java
@@ -0,0 +1,13 @@
+package com.csb.simplemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csb/simplemall/SimplemallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csb.simplemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplemallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
